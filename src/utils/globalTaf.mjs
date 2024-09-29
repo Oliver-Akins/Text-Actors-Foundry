4a1469ad70805e26e42db61778683e8ec636cfd8
Oliver-Akins <Oliver.akins1999@gmail.com>
2024-09-29 00:18:23 -0600

    Prevent overwriting the global taf object

--- src/utils/globalTaf.mjs
+++ src/utils/globalTaf.mjs
@@ -1,11 +1,18 @@
 import { hideMessageText } from "./feature_flags/rollModeMessageContent.mjs";
 
-globalThis.taf = Object.freeze({
-	utils: Object.freeze({
-		hideMessageText,
-	}),
-	FEATURES: {
-		ROLL_MODE_CONTENT: false,
-		STORABLE_SHEET_SIZE: false,
+Object.defineProperty(
+	globalThis,
+	`taf`,
+	{
+		value: Object.freeze({
+			utils: Object.freeze({
+				hideMessageText,
+			}),
+			FEATURES: Object.preventExtensions({
+				ROLL_MODE_CONTENT: false,
+				STORABLE_SHEET_SIZE: false,
+			}),
+		}),
+		writable: false,
 	},
-});
+);
